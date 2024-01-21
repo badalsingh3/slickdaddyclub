4756f1762dcad5c325eeb885bbe0a68e47673a4e
badalsingh3 <157306260+badalsingh3@users.noreply.github.com>
2024-01-21 22:49:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+﻿function login() {
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    // Add your logic to check the username and password
+    if (username === 'admin' && password === 'admin') {
+        // Redirect to another page on successful login
+        window.location.href = 'success.html';
+    } else {
+        document.getElementById('message').innerHTML = 'Incorrect username or password';
+    }
+}
